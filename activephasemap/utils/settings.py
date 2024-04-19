a706a86e9e793e9d1a3cd6ffb786b472f026e533
Kiran Vaddi <kiranvad@klone-login03.hyak.local>
2024-04-19 08:53:01 -0700

    add basic assertions to autophasemap dataset

--- activephasemap/utils/settings.py
+++ activephasemap/utils/settings.py
@@ -111,13 +111,16 @@ def get_twod_grid(n_grid, bounds):
 
 # Define a autophasemap dataset object
 class AutoPhaseMapDataSet(BaseDataSet):
-    def __init__(self, C, q, Iq, n_domain = 100):
-        super().__init__(n_domain=n_domain)
+    def __init__(self, C, q, Iq):
+        super().__init__(n_domain=q.shape[0])
         self.t = np.linspace(0,1, num=self.n_domain)
         self.q = q
         self.N = C.shape[0]
         self.Iq = Iq
-        self.C = C
+        self.C = C 
+
+        assert self.N==self.Iq.shape[0], "C and Iq should have same number of rows"
+        assert self.n_domain==self.Iq.shape[1], "Length of q should match with columns size of Iq"
     
     def generate(self, process=None):
         if process=="normalize":
@@ -126,5 +129,7 @@ class AutoPhaseMapDataSet(BaseDataSet):
             self.F = [self._smoothen(self.Iq[i,:]/self.l2norm(self.q, self.Iq[i,:]), window_length=7, polyorder=3) for i in range(self.N)]
         elif process is None:
             self.F = [self.Iq[i,:] for i in range(self.N)]
-            
+
+        assert len(self.F)==self.N, "Total number of functions should match the self.N"    
+
         return
