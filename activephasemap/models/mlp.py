a870072730a1020f64a48341fd9594a8991520c0
Kiran Vaddi <kiranvad@klone-login03.hyak.local>
2024-09-13 11:24:35 -0700

    add scalers to mlp to see if that improves

--- activephasemap/models/mlp.py
+++ activephasemap/models/mlp.py
@@ -1,21 +1,20 @@
 import torch
 import numpy as np 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+import pdb 
 
 class MLPModel(torch.nn.Module):
     def __init__(self, x_dim, z_dim):
         super().__init__()
-        layers = [torch.nn.Linear(x_dim, 16),
-                  torch.nn.ReLU(),
-                  torch.nn.Linear(16, 32),
-                  torch.nn.ReLU(),
-                  torch.nn.Linear(32, 16),
-                  torch.nn.ReLU()
+        layers = [torch.nn.Linear(x_dim, 128),
+                  torch.nn.Sigmoid(),
+                  torch.nn.Linear(128, 128),
+                  torch.nn.Sigmoid(),
                   ]
 
         self.x_to_hidden = torch.nn.Sequential(*layers)
-        self.hidden_to_mu = torch.nn.Linear(16, z_dim)
-        self.hidden_to_std = torch.nn.Linear(16, z_dim)
+        self.hidden_to_mu = torch.nn.Linear(128, z_dim)
+        self.hidden_to_std = torch.nn.Linear(128, z_dim)
 
     def forward(self, x):
         h = self.x_to_hidden(x)
@@ -44,21 +43,26 @@ class MLP(torch.nn.Module):
 
     def fit(self, use_early_stoping=True):
         optimizer = torch.optim.Adam(self.mlp.parameters(), lr=self.learning_rate)
-
+        self.mu_scaler = StandardScaler()
+        self.std_scaler = StandardScaler()
         train_ind = np.random.randint(0, len(self.c), int(0.8*len(self.c)))
         test_ind = np.setdiff1d(np.arange(len(self.c)), train_ind)
         self.train_c, self.train_z_mu, self.train_z_std = self.c[train_ind,:], self.z_mu[train_ind,:], self.z_std[train_ind,:]
+        self.train_z_mu  = self.mu_scaler.fit_transform(self.train_z_mu) 
+        self.train_z_std = self.std_scaler.fit_transform(self.train_z_std)
         self.test_c, self.test_z_mu, self.test_z_std = self.c[test_ind,:], self.z_mu[test_ind,:], self.z_std[test_ind,:]
+        self.test_z_mu  = self.mu_scaler.transform(self.test_z_mu) 
+        self.test_z_std = self.std_scaler.transform(self.test_z_std)
 
         train_dataset = torch.utils.data.TensorDataset(self.train_c, self.train_z_mu, self.train_z_std)
-        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=8, shuffle=True)
+        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=4, shuffle=True)
 
         eval_dataset = torch.utils.data.TensorDataset(self.test_c, self.test_z_mu, self.test_z_std)
         eval_loader = torch.utils.data.DataLoader(eval_dataset, batch_size=4, shuffle=False)
 
         self.mlp.train()
         train_loss, eval_loss = [], []
-        early_stopper = EarlyStopper(patience=100, min_delta=0.01)
+        early_stopper = EarlyStopper(patience=100, min_delta=0.1)
         for epoch in range(self.num_epochs):
             epoch_train_loss = []
             for xb, yb_mu, yb_std in train_loader:
@@ -84,7 +88,8 @@ class MLP(torch.nn.Module):
                 print(
                     f"Epoch {epoch+1:>3}/{self.num_epochs} - Loss: {train_loss[-1]:>4.3f} ", end=""
                 )
-                print(f" Evaluation Loss: {eval_loss[-1]:>4.3f} ")
+                print(f" Evaluation Loss: {eval_loss[-1]:>4.3f} ", end="")
+                print(" Early stopping counter: %d"%early_stopper.counter)
 
             if use_early_stoping and early_stopper.early_stop(eval_loss[-1]):
                 print("Early stopping...")
@@ -112,3 +117,35 @@ class EarlyStopper:
             if self.counter >= self.patience:
                 return True
         return False
+
+class StandardScaler:
+
+    def __init__(self, mean=None, std=None, epsilon=1e-7):
+        """Standard Scaler.
+        The class can be used to normalize PyTorch Tensors using native functions. The module does not expect the
+        tensors to be of any specific shape; as long as the features are the last dimension in the tensor, the module
+        will work fine.
+        :param mean: The mean of the features. The property will be set after a call to fit.
+        :param std: The standard deviation of the features. The property will be set after a call to fit.
+        :param epsilon: Used to avoid a Division-By-Zero exception.
+
+        from : https://gist.github.com/aryan-f/8a416f33a27d73a149f92ce4708beb40
+        """
+        self.mean = mean
+        self.std = std
+        self.epsilon = epsilon
+
+    def fit(self, values):
+        dims = list(range(values.dim() - 1))
+        self.mean = torch.mean(values, dim=dims)
+        self.std = torch.std(values, dim=dims)
+
+    def transform(self, values):
+        return (values - self.mean) / (self.std + self.epsilon)
+
+    def fit_transform(self, values):
+        self.fit(values)
+        return self.transform(values)
+    
+    def inverse_transform(self, values):
+        return (self.std + self.epsilon)*(values) + self.mean 
--- activephasemap/utils/acquisition.py
+++ activephasemap/utils/acquisition.py
@@ -49,25 +49,27 @@ class CompositeModelUncertainity(torch.nn.Module):
 
     def forward(self, x):
         z_mu, z_std = self.c_to_z.mlp(x)
+        z_mu = self.c_to_z.mu_scaler.inverse_transform(z_mu)
+        z_std = self.c_to_z.std_scaler.inverse_transform(z_std)        
         nr, nb, d = z_mu.shape
         z_dist = torch.distributions.Normal(z_mu, z_std)
-        z = z_dist.rsample(torch.Size([self.nz])).reshape(self.nz*nr*nb, d)
+        z = z_dist.rsample(torch.Size([self.nz])).view(self.nz*nr*nb, d)
         t = torch.from_numpy(self.t).repeat(self.nz*nr*nb, 1, 1).to(device)
         t = torch.swapaxes(t, 1, 2)
         y_samples, _ = self.z_to_y.xz_to_y(t, z)
-        sigma_pred = y_samples.reshape(self.nz, nr, nb, len(self.t), 1).std(dim=0)
+        sigma_pred = y_samples.view(self.nz, nr, nb, len(self.t), 1).std(dim=0)
         acqv = sigma_pred.mean(dim=-2).squeeze(-1)
 
         return acqv
 
-    def optimize(self, batch_size, num_restarts=8):
+    def optimize(self, batch_size, num_restarts=8, n_iterations=100):
         X = torch.rand(num_restarts, batch_size, len(self.bounds)).to(device)
         X = self.bounds[0] + (self.bounds[1] - self.bounds[0]) * X
         X.requires_grad_(True)
         optimizer = torch.optim.Adam([X], lr=0.05)
 
         start = time.time()
-        for i in range(100):
+        for i in range(n_iterations):
             optimizer.zero_grad()
             acqv = -self(X)
             loss = acqv.sum()
@@ -83,4 +85,4 @@ class CompositeModelUncertainity(torch.nn.Module):
                 time_str =  str(datetime.timedelta(seconds=end-start))
                 print(f"({time_str:>s}) Iteration {i+1:>3}/100 - Loss: {loss.item():>4.3f}")
         
-        return X[acqv.sum(dim=1).argmin(),...].detach()
+        return X[acqv.sum(dim=1).argmin(),...].clone().detach()
